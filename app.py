f44736d2d23e16e90d03a598a25de41436aebf1a
kkiillee55 <zhangliao322@icloud.com>
2020-10-13 21:38:06 -0400

    add code to app.py add home.html template add requirements.txt add a line in home.html add database RDS connection

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask,render_template
-
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+import json
 app=Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://ambrose:12345678@ec2testdb.cdl6fshxaa2x.us-east-2.rds.amazonaws.com/ec2schema'
+db=SQLAlchemy(app)
+# c_info={
+#     'host':'ec2testdb.cdl6fshxaa2x.us-east-2.rds.amazonaws.com',
+#     'user':'ambrose',
+#     'password':'12345678',
+#     'cursorclass':pymysql.cursors.DictCursor,
+# }
+# conn=pymysql.connect(**c_info)
+# cur=conn.cursor()
+# res=cur.execute('show databases;')
+# res=cur.fetchall()
+# print('database: ',json.dumps(res,indent=4,default=str))
 @app.route('/')
 def home():
-    return render_template('home.html')
+    data=db.session.execute('select * from ec2demo;')
+    return render_template('home.html',data=data)
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run(debug=True)
