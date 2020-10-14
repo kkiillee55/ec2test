556212a195453ff1de0e6ffb7ee4c36073bba147
kkiillee55 <zhangliao322@icloud.com>
2020-10-13 22:13:20 -0400

    add code to app.py add home.html template add requirements.txt add a line in home.html add database RDS connection update requirements.txt

--- app.py
+++ app.py
@@ -2,8 +2,23 @@ from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
 import json
+import os
+'''
+for windows: 
+set username=your username
+set password=your password
+set dbserver=your dbserver
+
+for linux
+change set to export
+'''
+username=os.environ['username']
+password=os.environ['password']
+dbserver=os.environ['dbserver']
+print('mysql+pymysql://{}:{}@{}'.format(username,password,dbserver))
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://ambrose:12345678@ec2testdb.cdl6fshxaa2x.us-east-2.rds.amazonaws.com/ec2schema'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://{}:{}@{}'.format(username,password,dbserver)
+#app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://ambrose:12345678@ec2testdb.cdl6fshxaa2x.us-east-2.rds.amazonaws.com/ec2schema'
 db=SQLAlchemy(app)
 # c_info={
 #     'host':'ec2testdb.cdl6fshxaa2x.us-east-2.rds.amazonaws.com',
@@ -16,10 +31,11 @@ db=SQLAlchemy(app)
 # res=cur.execute('show databases;')
 # res=cur.fetchall()
 # print('database: ',json.dumps(res,indent=4,default=str))
+
 @app.route('/')
 def home():
     data=db.session.execute('select * from ec2demo;')
     return render_template('home.html',data=data)
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run(debug=True)
